b2e6509c403c9e43e0babc3d0830077f9e3d728c
DRaczek <damian.raczek4206695@gmail.com>
2025-08-09 15:46:37 +0200

    Camera controller timeline optimization fix

--- src/components/CameraController.jsx
+++ src/components/CameraController.jsx
@@ -1,31 +1,40 @@
-import { useThree, useFrame } from "@react-three/fiber";
+import { useThree } from "@react-three/fiber";
 import gsap from "gsap";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 export function CameraController() {
   const { camera } = useThree();
+  const tl = useRef(null);
 
   useEffect(() => {
     camera.position.set(7.5, 0.3, 7.5);
     camera.lookAt(0, 2, 0);
-    const tl = gsap.timeline({
+
+    tl.current = gsap.timeline({
       repeat: -1,
       defaults: { ease: "none" },
     });
 
-    tl.to(camera.position, {
+    tl.current.to(camera.position, {
       duration: 30,
       x: 6.1,
       y: 0.3,
       z: 6.1,
     });
 
-    tl.to(camera.position, {
+    tl.current.to(camera.position, {
       duration: 5,
       x: 7.5,
       y: 0.3,
       z: 7.5,
     });
+
+    return () => {
+      if (tl.current) {
+        tl.current.kill();
+        tl.current = null;
+      }
+    };
   }, [camera]);
 
   return null;
